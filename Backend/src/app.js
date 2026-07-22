f93e6016e81bde62d3ea83b2d89a5751ab383f63
anshpatel1502-debug <anshpatel1502@gmail.com>
2026-07-22 14:39:54 +0530

    ad

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,6 +4,7 @@ const cors = require("cors")
 
 const app = express()
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 console.log(process.env.FRONTEND_URL);
 app.use(cors({    
